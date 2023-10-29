0ba0c96286ba5f6acf442ddd175c5a1471215063
Qhaleed <139474360+Qhaleed@users.noreply.github.com>
2023-10-29 20:52:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,37 @@ listContainer.addEventListener("click", function(e) {
     }
     else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
-    }
+    } 
 }
 )
+
+window.addEventListener('load', () => {
+    const savedTasks = localStorage.getItem('tasks');
+
+    if (savedTasks) {
+        const tasks = JSON.parse(savedTasks);
+
+        tasks.forEach(task => {
+            createTaskElement(task);
+        });
+    }
+});
+
+function createTaskElement(taskText) {
+    const li = document.createElement("li");
+    li.innerHTML = taskText;
+    listContainer.appendChild(li);
+
+    const span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
+    // Store the updated list of tasks in local storage
+    saveTasksToLocalStorage();
+}
+
+function saveTasksToLocalStorage() {
+    const tasks = Array.from(listContainer.getElementsByTagName("li")).map(li => li.innerHTML);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
